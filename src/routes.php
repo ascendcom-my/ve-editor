accfcfeacd93bc546b80477051cbb496350b2b5e
Isaac Mok <isaac.mok.kx@gmail.com>
2020-10-22 11:52:59 +0800

    fix

--- src/routes.php
+++ src/routes.php
@@ -11,8 +11,8 @@ use Bigmom\VeEditor\Http\Controllers\TagController;
 use Bigmom\VeEditor\Http\Middleware\EnsureUserIsAuthorized;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('ve-editor')->name('ve-editor.')->middleware(['auth:sanctum', EnsureUserIsAuthorized::class])->group(function () {
-    Route::middleware(['web'])->group(function () {
+Route::prefix('ve-editor')->name('ve-editor.')->group(function () {
+    Route::middleware(['web', 'auth:sanctum', EnsureUserIsAuthorized::class])->group(function () {
         if (config('ve.main')) {
             Route::get('/', function () {
                 return redirect()->route('ve-editor.scene.getIndex');
@@ -48,7 +48,7 @@ Route::prefix('ve-editor')->name('ve-editor.')->middleware(['auth:sanctum', Ensu
             Route::post('/pull', [ContentController::class, 'pull'])->name('pull');
         }
     });
-    Route::middleware(['api'])->prefix('api')->name('api.')->group(function () {
+    Route::middleware(['api', 'auth:sanctum', EnsureUserIsAuthorized::class])->prefix('api')->name('api.')->group(function () {
         if (config('ve.main')) {
             Route::get('/pull', [APIController::class, 'getContent'])->name('getContent');
         }
